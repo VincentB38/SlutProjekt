9dd8cd75fb7e7138100f8fe064c402c8783fcda1
VincentB38 <vincent.blomberg@elev.ga.lbs.se>
2026-04-28 11:16:26 +0200

    done stuff to level menu, need 1 more lesson for it

--- TestSlutProjekt/Assets/Scripts/Extras/LevelMapManager.cs
+++ TestSlutProjekt/Assets/Scripts/Extras/LevelMapManager.cs
@@ -9,11 +9,11 @@ public class LevelMapManager : MonoBehaviour
     {
 
         int levelIndex = PlayerPrefs.GetInt("MaxLevel", 0); // get the max level and if it does not exist then 0
-
+        levelIndex = 0;
         // unlock levels
         for (int i = 0; i < levelParts.Length; i++) // Loop through
         {
-            if (i >= levelIndex) // if i is more or equal to level index means you unlocked it
+            if (levelIndex > i) // if level index is more than i then that means you unlocked it
                 levelParts[i].Unlock(); // disable the lock
         }
 
--- TestSlutProjekt/Assets/Scripts/Extras/LevelPart.cs
+++ TestSlutProjekt/Assets/Scripts/Extras/LevelPart.cs
@@ -1,13 +1,20 @@
+using TMPro;
 using UnityEngine;
 
 public class LevelPart : MonoBehaviour
 {
-    //public GameObject lockIcon;
+    public GameObject lockIcon; // the lock icon
+    public TextMeshProUGUI LevelText; // for the text
 
+    private void Start()
+    {
+        LevelText.enabled = false; // disable the text in the start
+    }
     public void Unlock()
     {
-       // if (lockIcon != null)
-          //  lockIcon.SetActive(false);
+        if (lockIcon != null) 
+            lockIcon.SetActive(false); // make it disabled (meaning the player unlocked this stage)
+            LevelText.enabled = true; // show the level
 
     }
 }
