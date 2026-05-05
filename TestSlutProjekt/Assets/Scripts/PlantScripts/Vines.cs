e299d2d9115c9ec2543f6817296c16850938dda3
FedericoQB <federico.quijano@outlook.com>
2026-05-05 10:59:21 +0200

    Added Vines function

---
+++ TestSlutProjekt/Assets/Scripts/PlantScripts/Vines.cs
@@ -0,0 +1,62 @@
+using System.Collections.Generic;
+using System.Collections;
+using Unity.Hierarchy;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Vines : SpecialOffensePlant
+{
+    public float slownessMultiplier;
+    public float radius;
+    public float damageCooldown = 1;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            collision.GetComponent<EnemyHandler>().Speed *= slownessMultiplier; // Changes the enemy's speed by multiplying with the slownessMulti.
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            collision.GetComponent<EnemyHandler>().Speed /= slownessMultiplier; // Restores slowed speed to original speed after leaving trigger
+        }
+    }
+
+    IEnumerator DamageEnemies()
+    {
+        while (true)
+        {
+            Vector2 position = transform.position;
+            LayerMask mask = LayerMask.GetMask("EnemyLayer");
+            Collider2D[] hits = Physics2D.OverlapCircleAll(position, radius, mask);
+
+            foreach (Collider2D hit in hits) // Goes through hits array
+            {
+                EnemyHandler enemy = hit.GetComponent<EnemyHandler>(); // gets the enemy script
+                if (enemy != null)
+                // makes sure enemy isn't null
+                {
+                    enemy.ChangeHealth(-1); // negative as negative and negative is positive
+                }
+            }
+
+            yield return new WaitForSeconds(damageCooldown);
+        }
+    }
+}
